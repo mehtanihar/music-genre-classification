b1713e3117d4cd0122a8d71f2abdadab1a63f07e
mehtanihar <niharmehta79@gmail.com>
2016-11-21 21:12:05 +0530

    Music Genre Classification

---
+++ README.md
@@ -0,0 +1,34 @@
+Music Genre Classification using Convolutional Recurrent Neural Networks
+
+
+Meet Shah (13D070003)
+
+Nihar Mehta (13D100011)
+
+Sarthak Daga (130070015)
+
+Saumil Shah (13D170001)
+
+
+Github link: https://github.com/meetshah1995/crnn-music-genre-classification
+
+Marsyas link: http://marsyasweb.appspot.com/download/data_sets/
+
+
+Instructions: 
+
+Data: Download GENRE data from Marsyas. Extract the tarball in the directory which contains the cloned repo.
+
+Running the code
+
+CNN
+
+python cnn.py
+
+CRNN
+
+python crnn.py
+
+Dependencies:
+
+scikit image, scikit learn, librosa, hidden markov model (hmm) , scipy# music-genre-classification
---
+++ src/data_preprocess.py
@@ -0,0 +1,36 @@
+import sys
+import numpy as np
+import librosa as lb
+
+from scipy import misc
+
+Fs         = 12000#sampling rate
+N_FFT      = 512#length of fft window
+N_MELS     = 96
+N_OVERLAP  = 256
+DURA       = 29.12
+
+#hop length:no of samples between successive frames S:power spectrogram y: audio time series
+
+
+def log_scale_melspectrogram(path, plot=False):
+    signal, sr = lb.load(path, sr=Fs)
+    n_sample = signal.shape[0]
+    n_sample_fit = int(DURA*Fs)
+
+    if n_sample < n_sample_fit:
+        signal = np.hstack((signal, np.zeros((int(DURA*Fs) - n_sample,))))
+    elif n_sample > n_sample_fit:
+        signal = signal[(n_sample-n_sample_fit)/2:(n_sample+n_sample_fit)/2]
+
+    melspect = lb.logamplitude(lb.feature.melspectrogram(y=signal, sr=Fs, hop_length=N_OVERLAP, n_fft=N_FFT, n_mels=N_MELS)**2, ref_power=1.0)
+
+    if plot:
+        melspect = melspect[np.newaxis, :]
+        misc.imshow(melspect.reshape((melspect.shape[1],melspect.shape[2])))
+        print(melspect.shape)
+
+    return melspect
+
+if __name__ == '__main__':
+    log_scale_melspectrogram(sys.argv[1],True)
